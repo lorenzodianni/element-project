4255c12ee8531da612ff1115494c5f4aa6ab2b01
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2016-12-02 00:09:16 +0100

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from "rollup-plugin-babel";
+import commonjs from 'rollup-plugin-commonjs';
+import npm from 'rollup-plugin-node-resolve';
+
+export default {
+  entry: './src/index.js',
+  moduleName: 'El',
+  targets: [
+    {dest: 'dist/elent.js', format: 'umd'}
+  ],
+  plugins: [
+    babel({exclude: 'node_modules/**'}),
+    npm({main: true, jsnext: true, browser: true}),
+    commonjs()
+  ]
+}
